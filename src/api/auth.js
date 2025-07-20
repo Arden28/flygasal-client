b8bd2f227cfb4e6efa7aa59d2d640fa5b12f2ae2
Arden28 <laudbouetoumoussa@gmail.com>
2025-07-20 07:23:02 +0100

    Updated api baseUrl

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://flygasal.koverae.com/api",
 });
 
 // Utility to get token from localStorage
