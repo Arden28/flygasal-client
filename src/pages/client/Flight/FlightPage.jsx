e46c5ef53b724fda192af987692b4d57102d0ee9
Arden28 <laudbouetoumoussa@gmail.com>
2025-10-08 10:50:58 +0100

    Added icons

--- src/pages/client/Flight/FlightPage.jsx
+++ src/pages/client/Flight/FlightPage.jsx
@@ -402,6 +402,7 @@ const FlightPage = () => {
 
         const res = await flygasal.searchFlights(params, { signal: abort.signal });
 
+        console.info('Search results: ', res);
         let offers = [];
         let displayCurrency = "USD";
 
